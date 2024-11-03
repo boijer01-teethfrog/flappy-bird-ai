da65a5aa92666361d92955c655ccc70b8eae5154
Simon Boijer <boijersi@arcada.fi>
2024-11-04 01:09:10 +0200

    added falling effect to the player

--- Assets/BirdAgent.cs
+++ Assets/BirdAgent.cs
@@ -6,6 +6,7 @@ public class BirdAgent : MonoBehaviour
 {
     public float jumpVelocity = 5f; 
     private Rigidbody2D rb;
+    public float rotation = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +21,21 @@ public class BirdAgent : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
         }
+        UpdateRotation();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("pipe"))
         {
-            Debug.Log("COLLIDED WITH PIPE D:"); //ändra det här att restarta å börja ny episod
+            Debug.Log("COLLIDED WITH PIPE D:"); //ändra det här att restarta å börja ny episod när vi fixar agenten sen
         }
+    }
+
+    void UpdateRotation()
+    {
+        float fallSpeed = rb.velocity.y;
+        float rotationAngle = Mathf.Clamp(fallSpeed * rotation, -90f, 45f); 
+        transform.rotation = Quaternion.Euler(0, 0, rotationAngle);
     }
 }
